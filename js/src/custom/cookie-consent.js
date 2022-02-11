24006c4cc1041fac98dc408dd85eccf9f5ab2e0e
Benedict <ben@benedict-wallis.com>
2022-02-11 17:35:54 +0000

    Adds cookie for backend processing

--- js/src/custom/cookie-consent.js
+++ js/src/custom/cookie-consent.js
@@ -1,17 +1,30 @@
-const consentEl = document.getElementsByClassName('cookie-consent')[0];
+const consentEl = document.getElementsByClassName("cookie-consent")[0];
 
 if (consentEl) {
 
-	if (localStorage.getItem('cookie-consent')) {
+	const now = new Date();
+
+	if (localStorage.getItem("cookie-consent").expiry < now.valueOf()) {
 		consentEl.remove();
 	}
 
-	consentEl.getElementsByTagName('button')[0].addEventListener('click', function () {
+	consentEl.getElementsByTagName("button")[0].addEventListener("click", function () {
 
-		localStorage.setItem('cookie-consent', true);
+		const expiry = now.setTime(now.getTime() + (28 * 24 * 60 * 60 * 1000));
 
-		consentEl.style.opacity = '0';
-		consentEl.addEventListener('transitionend', () => consentEl.remove());
+		const item = {
+			value: true,
+			expiry: expiry,
+		};
+
+		localStorage.setItem("cookie-consent", JSON.stringify(item));
+
+		// drop a cookie for the backend
+		document.cookie = "cookie-consent=true;expires=" + expiry + ";path=/";
+
+		// remove the consent element
+		consentEl.style.opacity = "0";
+		consentEl.addEventListener("transitionend", () => consentEl.remove());
 
 		return false;
 	});
