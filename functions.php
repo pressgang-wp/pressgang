68244fe677d3263dcb469f318c6ba83f72b84244
Benedict <ben@benedict-wallis.com>
2022-04-11 10:27:00 +0100

    Remove autoload

--- functions.php
+++ functions.php
@@ -4,8 +4,9 @@ if ( ! defined( 'THEMENAME' ) ) {
 	define( 'THEMENAME', 'pressgang' );
 }
 
+// TODO this needs to be done in the child theme - could get child theme directory here?
 // Ensure composer is auto loaded
-require_once( __DIR__ . '/vendor/autoload.php' );
+// require_once( __DIR__ . '/vendor/autoload.php' );
 
 /**
  * Go!
