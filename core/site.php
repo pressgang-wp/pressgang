1dfc621d123f646600e63feab4db8c720c8eb86c
Benedict <ben@benedict-wallis.com>
2023-01-04 11:55:54 +0000

    Improved meta description for terms
    Needs fix for Yoast meta

--- core/site.php
+++ core/site.php
@@ -15,6 +15,7 @@ require_once( 'loader.php' );
  * @package PressGang
  */
 class Site extends \Timber\Site {
+
 	public $stylesheet;
 	public $keywords;
 	public $logo;
@@ -24,7 +25,7 @@ class Site extends \Timber\Site {
 	/**
 	 *__construct
 	 *
-	 * @param string|int $site_name_or_id
+	 * @param  string|int  $site_name_or_id
 	 */
 	function __construct( $site_name_or_id = null ) {
 		// load all customizer mods
@@ -59,22 +60,26 @@ class Site extends \Timber\Site {
 
 		// get stylesheet (can be set in customizer theme mod)
 		$this->stylesheet = $this->stylesheet ?: 'styles.css';
-		$this->stylesheet = sprintf( "%s/css/%s?v=%s", get_stylesheet_directory_uri(), $this->stylesheet, filemtime( get_stylesheet_directory() . "/css/{$this->stylesheet}" ) );
+		$this->stylesheet = sprintf( "%s/css/%s?v=%s",
+			get_stylesheet_directory_uri(), $this->stylesheet,
+			filemtime( get_stylesheet_directory() . "/css/{$this->stylesheet}" ) );
 
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+		add_filter( 'timber_context', [ $this, 'add_to_context' ] );
+		add_filter( 'get_twig', [ $this, 'add_to_twig' ] );
 
-		add_filter( 'meta_description', array( 'PressGang\Site', 'meta_description' ) );
+		add_filter( 'meta_description',
+			[ 'PressGang\Site', 'meta_description' ] );
 
 		// add_filter('wp_headers', array($this, 'add_ie_header'));
 
 		if ( class_exists( 'WooCommerce' ) ) {
-			add_filter( 'timber_context', array( $this, 'add_woocommerce_to_context' ) );
+			add_filter( 'timber_context',
+				[ $this, 'add_woocommerce_to_context' ] );
 		}
 
 		// switch on twig caching if production
 		if ( class_exists( 'Timber' ) ) {
-			Timber::$cache = \defined('TIMBER_CACHE') ? TIMBER_CACHE : !WP_DEBUG;
+			Timber::$cache = \defined( 'TIMBER_CACHE' ) ? TIMBER_CACHE : ! WP_DEBUG;
 		}
 
 		// add a theme color
@@ -133,26 +138,30 @@ class Site extends \Timber\Site {
 	public function add_to_twig( $twig ) {
 		$twig->addFunction( new \Twig\TwigFunction( 'esc_attr', 'esc_attr' ) );
 		$twig->addFunction( new \Twig\TwigFunction( 'esc_url', 'esc_url' ) );
-		$twig->addFunction( new \Twig\TwigFunction( 'get_search_query', 'get_search_query' ) );
+		$twig->addFunction( new \Twig\TwigFunction( 'get_search_query',
+			'get_search_query' ) );
 
-		$twig->addFunction( new \Twig\TwigFunction( 'meta_description', array(
+		$twig->addFunction( new \Twig\TwigFunction( 'meta_description', [
 			'PressGang\Site',
-			'meta_description'
-		) ) );
+			'meta_description',
+		] ) );
 
-		$twig->addFunction( new \Twig\TwigFunction( 'get_option', 'get_option' ) );
-		$twig->addFunction( new \Twig\TwigFunction( 'get_theme_mod', 'get_theme_mod' ) );
+		$twig->addFunction( new \Twig\TwigFunction( 'get_option',
+			'get_option' ) );
+		$twig->addFunction( new \Twig\TwigFunction( 'get_theme_mod',
+			'get_theme_mod' ) );
 
 		if ( class_exists( 'WooCommerce' ) ) {
-			$twig->addFunction( new \Twig\TwigFilter( 'timber_set_product', array(
+			$twig->addFunction( new \Twig\TwigFilter( 'timber_set_product', [
 				'PressGang\Site',
-				'timber_set_product'
-			) ) );
+				'timber_set_product',
+			] ) );
 		}
 
 		// TODO can we lazy load or include?
 
-		$twig->addFilter( new \Twig\TwigFilter( 'pluralize', array( 'PressGang\Pluralizer', 'pluralize' ) ) );
+		$twig->addFilter( new \Twig\TwigFilter( 'pluralize',
+			[ 'PressGang\Pluralizer', 'pluralize' ] ) );
 
 		// add text-domain to global
 		$twig->addGlobal( 'THEMENAME', THEMENAME );
@@ -167,58 +176,77 @@ class Site extends \Timber\Site {
 	 *
 	 */
 	public static function meta_description() {
+
 		$description = self::$meta_description;
 
 		if ( empty( $description ) ) {
 
-			$post = new TimberPost();
-
-			$key = sprintf( "meta_description_%d", $post->ID );
+			$object = get_queried_object();
+			$key    = sprintf( "meta_description_%s_%d",
+				strtolower( get_class( $object ) ), $object->ID );
 
 			if ( ! $description = wp_cache_get( $key ) ) {
 
-				// try Yoast
-				$description = get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
+				if ( is_single() || is_page() ) {
 
-				if ( ! $description ) {
+					// try Yoast
+					$description = get_post_meta( $object->ID,
+						'_yoast_wpseo_metadesc', true );
 
-					// check for custom field
-					$description = wptexturize( $post->get_field( 'meta_description' ) );
+					if ( empty( $description ) ) {
 
-					if ( is_tax() ) {
-						if ( $temp = term_description( get_queried_object(), get_query_var( 'taxonomy' ) ) ) {
+						$post = new TimberPost();
+
+						// check for custom field
+						$description = wptexturize( $post->get_field( 'meta_description' ) );
+
+						// else use excerpt
+						if ( empty( $description ) ) {
+							$description = $post->post_excerpt;
+						}
+
+						// else use preview
+						if ( empty( $description ) ) {
+							$description = strip_shortcodes( $post->post_content );
+						}
+					}
+
+				}
+
+				if ( is_tax() ) {
+
+					// try Yoast
+					// TODO - bug https://github.com/Yoast/wordpress-seo/issues/12548
+
+					$description = get_term_meta( $object->ID,
+						'_yoast_wpseo_metadesc', true );
+
+					if ( empty( $description ) ) {
+
+						if ( $temp = term_description( get_queried_object(),
+							get_query_var( 'taxonomy' ) ) ) {
 							$description = $temp;
 						}
-					} elseif ( is_post_type_archive() ) {
-						if ( $temp = get_the_archive_description() ) {
-							$description = $temp;
-						}
 					}
 
-					// else use excerpt
-					if ( empty( $description ) ) {
-						$description = $post->post_excerpt;
+				} elseif ( is_post_type_archive() ) {
+					if ( $temp = get_the_archive_description() ) {
+						$description = $temp;
 					}
+				}
 
-					// else use preview
-					if ( empty( $description ) ) {
-						// TODO get_preview is rendering block content
-						// $description = $post->get_preview(50, false, false, true);
-						$description = wp_strip_all_tags( strip_shortcodes( $post->post_content ) );
-					}
+				// finally use the blog description
+				if ( empty( $description ) ) {
+					$description = get_bloginfo( 'description', 'raw' );
+				}
 
-					// finally use the blog description
-					if ( empty( $description ) ) {
-						$description = get_bloginfo( 'description', 'raw' );
-					}
+				$description = esc_attr( wp_strip_all_tags( $description ) );
 
-					$description = esc_attr( wp_strip_all_tags( $description ) );
-
-					// limit to SEO recommended length
-					if ( strlen( $description ) > 155 ) {
-						$description = mb_substr( $description, 0, 155 );
-						$description = TimberHelper::trim_words( $description, str_word_count( $description ) - 1 );
-					}
+				// limit to SEO recommended length
+				if ( strlen( $description ) > 155 ) {
+					$description = mb_substr( $description, 0, 155 );
+					$description = TimberHelper::trim_words( $description,
+						str_word_count( $description ) - 1 );
 				}
 
 				self::$meta_description = $description;
@@ -234,10 +262,12 @@ class Site extends \Timber\Site {
 	 *
 	 * TODO BREAKS SITEGROUND DYNAMIC CACHE
 	 *
-	 * see - http://stackoverflow.com/questions/14198594/bad-value-x-ua-compatible-for-attribute-http-equiv-on-element-meta
+	 * see -
+	 * http://stackoverflow.com/questions/14198594/bad-value-x-ua-compatible-for-attribute-http-equiv-on-element-meta
 	 */
 	public function add_ie_header() {
-		if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && ( strpos( $_SERVER['HTTP_USER_AGENT'], 'MSIE' ) !== false ) ) {
+		if ( isset( $_SERVER['HTTP_USER_AGENT'] ) && ( strpos( $_SERVER['HTTP_USER_AGENT'],
+					'MSIE' ) !== false ) ) {
 			header( 'X-UA-Compatible: IE=edge,chrome=1' );
 		}
 	}
