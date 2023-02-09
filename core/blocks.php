1c4810c8a194029ad2ea2da272a355c240742f86
Benedict <ben@benedict-wallis.com>
2023-02-09 20:51:34 +0000

    Update to allow blocks.json in settings

--- core/blocks.php
+++ core/blocks.php
@@ -10,12 +10,13 @@ namespace PressGang;
  * @package PressGang
  */
 class Blocks {
+
 	/**
 	 * __construct
 	 *
 	 */
 	public function __construct() {
-		add_action( 'init', array( $this, 'setup' ) );
+		add_action( 'init', [ $this, 'setup' ] );
 	}
 
 	/**
@@ -24,44 +25,8 @@ class Blocks {
 	public function setup() {
 		$blocks = Config::get( 'blocks' );
 
-		foreach ( $blocks as $key => &$args ) {
-
-			$block = array();
-
-			if ( isset( $args['editor_script'] ) ) {
-
-				wp_register_script(
-					$args['editor_script']['handle'],
-					$args['editor_script']['src'],
-					$args['editor_script']['deps']
-				);
-
-				$block['editor_script'] = $args['editor_script']['handle'];
-			}
-
-			if ( isset( $args['editor_style'] ) ) {
-				wp_register_style(
-					$args['editor_style']['handle'],
-					$args['editor_style']['src'],
-					$args['editor_style']['deps'],
-					$args['editor_style']['version']
-				);
-
-				$block['editor_style'] = $args['editor_script']['handle'];
-			}
-
-			if ( isset( $args['style'] ) ) {
-				wp_register_style(
-					$args['style']['handle'],
-					$args['style']['src'],
-					$args['style']['deps'],
-					$args['style']['version']
-				);
-
-				$block['style'] = $args['style']['handle'];
-			}
-
-			register_block_type( $key, $block );
+		foreach ( $blocks as $key => &$path ) {
+			register_block_type( get_stylesheet_directory() . $path );
 		}
 	}
 
