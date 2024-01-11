e8f85bfbe8147c4092adea7184ff6a4b3fda5c66
Benedict <ben@benedict-wallis.com>
2024-01-11 17:47:09 +0000

    Update snake case functions, add doc blocks and PHP 8 function calls

--- src/ServiceProviders/TimberServiceProvider.php
+++ src/ServiceProviders/TimberServiceProvider.php
@@ -10,45 +10,72 @@ use PressGang\ContextManagers\ThemeModsContextManager;
 use PressGang\ContextManagers\WooCommerceContextManager;
 use PressGang\Twig\TwigExtensionManager;
 
+/**
+ * Class TimberServiceProvider
+ *
+ * Manages the setup and provisioning of services related to the Timber library in a WordPress theme.
+ * It handles the registration of various context managers and adds additional functionalities to the Twig environment.
+ *
+ * @package PressGang\ServiceProviders
+ */
 class TimberServiceProvider {
-	protected $contextManagers = [];
+	protected array $contextManagers = [];
 
-	public function boot() {
+	/**
+	 * Bootstraps the service provider by registering context managers and configuring Timber.
+	 *
+	 * This method sets up various managers to add data to the Timber context and configures Twig extensions.
+	 */
+	public function boot(): void {
 		// Register context managers
-		$this->registerContextManager( new GeneralContextManager() );
-		$this->registerContextManager( new MenuContextManager() );
+		$this->register_context_manager( new GeneralContextManager() );
+		$this->register_context_manager( new MenuContextManager() );
 
 		// Register Theme Mods
-		$this->registerContextManager( new ThemeModsContextManager() );
+		$this->register_context_manager( new ThemeModsContextManager() );
 
 		// Register ACF Options
 		if ( function_exists( 'get_fields' ) && config( 'acf-options' ) ) {
-			$this->registerContextManager( new AcfOptionsContextManager() );
+			$this->register_context_manager( new AcfOptionsContextManager() );
 		}
 
 		// Register WooCommerce
 		if ( class_exists( 'WooCommerce' ) ) {
-			$this->registerContextManager( new WooCommerceContextManager() );
+			$this->register_context_manager( new WooCommerceContextManager() );
 		}
 
 		// Add context filters
-		add_filter( 'timber/context', [ $this, 'addToContext' ] );
+		\add_filter( 'timber/context', [ $this, 'add_to_context' ] );
 
 		// Add to Twig functions
-		add_filter( 'timber/twig', function ( $twig ) {
+		\add_filter( 'timber/twig', function ( $twig ) {
 			$twigExtensionsManager = new TwigExtensionManager();
 
 			return $twigExtensionsManager->addToTwig( $twig );
 		} );
 	}
 
-	protected function registerContextManager( ContextManagerInterface $manager ) {
+	/**
+	 * Registers a context manager for adding data to the Timber context.
+	 *
+	 * @param ContextManagerInterface $manager The context manager to be registered.
+	 */
+	protected function register_context_manager( ContextManagerInterface $manager ): void {
 		$this->contextManagers[] = $manager;
 	}
 
-	public function addToContext( $context ) {
+	/**
+	 * Adds additional data to the Timber context using the registered context managers.
+	 *
+	 * Iterates through each registered context manager and applies its modifications to the Timber context.
+	 *
+	 * @param array $context The current Timber context array.
+	 *
+	 * @return array The modified Timber context array.
+	 */
+	public function add_to_context( $context ) {
 		foreach ( $this->contextManagers as $manager ) {
-			$context = $manager->addToContext( $context );
+			$context = $manager->add_to_context( $context );
 		}
 
 		return $context;
