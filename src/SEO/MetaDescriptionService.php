ea627ef7b8125b182a9135f6e40aa1d56a67fbf0
Benedict <ben@benedict-wallis.com>
2024-01-12 11:49:19 +0000

    Update to WordPress snake case

--- src/SEO/MetaDescriptionService.php
+++ src/SEO/MetaDescriptionService.php
@@ -6,44 +6,44 @@ use Timber\TextHelper;
 use Timber\Timber;
 
 class MetaDescriptionService {
-	private static $meta_description;
+	private static string $meta_description;
 
-	public static function getMetaDescription() {
+	public static function get_meta_description() {
 		if ( self::$meta_description !== null ) {
 			return self::$meta_description;
 		}
 
 		if ( $object = \get_queried_object() ) {
-			$key = self::generateCacheKey( $object );
+			$key = self::generate_cache_key( $object );
 
 			if ( ! self::$meta_description = \wp_cache_get( $key ) ) {
-				self::$meta_description = self::generateDescription( $object );
+				self::$meta_description = self::get_default_description( $object );
 				\wp_cache_set( $key, self::$meta_description );
 			}
 		} else {
 			self::$meta_description = \get_bloginfo( 'description', 'raw' );
 		}
 
-		return self::sanitizeAndShortenDescription( self::$meta_description );
+		return self::sanitize_and_shorten_description( self::$meta_description );
 	}
 
-	private static function generateCacheKey( $object ) {
+	private static function generate_cache_key( $object ): string {
 		return sprintf( "meta_description_%s_%s", strtolower( get_class( $object ) ), $object->ID ?? $object->name );
 	}
 
-	private static function generateDescription( $object ) {
+	private static function generate_description( $object ) {
 		if ( \is_single() || \is_page() ) {
-			return self::getDescriptionForPost( $object ) ?: self::getDefaultDescription();
+			return self::get_description_for_post( $object ) ?: self::get_default_description();
 		} elseif ( \is_tax() ) {
-			return self::getDescriptionForTaxonomy( $object ) ?: self::getDefaultDescription();
+			return self::get_description_for_taxonomy( $object ) ?: self::get_default_description();
 		} elseif ( \is_post_type_archive() ) {
-			return \get_the_archive_description() ?: self::getDefaultDescription();
+			return \get_the_archive_description() ?: self::get_default_description();
 		}
 
-		return self::getDefaultDescription();
+		return self::get_default_description();
 	}
 
-	private static function getDescriptionForPost( $post ) {
+	private static function get_description_for_post( $post ) {
 		// Try Yoast SEO first
 		$description = \get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true );
 
@@ -56,7 +56,7 @@ class MetaDescriptionService {
 		return $description;
 	}
 
-	private static function getDescriptionForTaxonomy( $term ) {
+	private static function get_description_for_taxonomy( $term ) {
 		// Try Yoast SEO for taxonomy
 		$yoast_meta = \get_option( 'wpseo_taxonomy_meta' );
 		if ( ! empty( $yoast_meta[ $term->taxonomy ][ $term->term_id ]['wpseo_desc'] ) ) {
@@ -66,11 +66,11 @@ class MetaDescriptionService {
 		return \term_description( $term, \get_query_var( 'taxonomy' ) );
 	}
 
-	private static function getDefaultDescription() {
+	private static function get_default_description() {
 		return \get_bloginfo( 'description', 'raw' );
 	}
 
-	private static function sanitizeAndShortenDescription( $description ) {
+	private static function sanitize_and_shorten_description( $description ) {
 		$description = \esc_attr( \wp_strip_all_tags( $description ) );
 
 		if ( strlen( $description ) > 155 ) {
