45176daa68e7f1e82801cec2f18be781887de2cd
Benedict <ben@benedict-wallis.com>
2024-01-12 16:08:05 +0000

    Add doc blocks

--- includes/editor-styles.php
+++ includes/editor-styles.php
@@ -1,26 +1,39 @@
 <?php
 
+/**
+ * Class EditorStyles
+ *
+ * Handles the addition of custom styles to the WordPress editor.
+ */
 class EditorStyles {
 
+	protected string $editor_styles_path = '/css/editor-styles.css';
+
 	/**
-	 * EditorStyle constructor.
+	 * EditorStyles constructor.
+	 *
+	 * Adds an action hook to initialize editor styles.
 	 */
-	public function __construct() {
-		add_action( 'admin_init', array( $this, 'add_editor_styles' ) );
+	public function __construct( array $args ) {
+
+		if ( isset( $args['path'] ) ) {
+			$this->editor_styles_path = $args['path'];
+		}
+
+		add_action( 'admin_init', [ $this, 'add_editor_styles' ] );
 	}
 
 	/**
-	 * add_editor_styles
+	 * Adds custom styles to the WordPress editor.
 	 *
-	 * https://codex.wordpress.org/Editor_Style
-	 *
-	 * @hooked init
+	 * This method enables theme support for editor styles and adds a specific stylesheet
+	 * to the editor to ensure consistency between the editor and front-end styles.
 	 */
 	public function add_editor_styles() {
 		add_theme_support( 'editor-styles' );
-		add_editor_style( '/css/editor-styles.css' );
+		add_editor_style( $this->editor_styles_path );
 	}
 
 }
 
-new EditorStyles();
+new EditorStyles([]);
