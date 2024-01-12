7cb6a92752922ef69a49c446e0f842d9bb0ca4f8
Benedict <ben@benedict-wallis.com>
2024-01-12 13:48:58 +0000

    Update admin-logo.php

--- includes/admin-logo.php
+++ includes/admin-logo.php
@@ -1,34 +1,53 @@
 <?php
 
+/**
+ * Class AdminLogo
+ *
+ * Customizes the WordPress admin login logo with the theme's logo.
+ */
 class AdminLogo {
 
 	/**
-	 * init
+	 * Constructor.
 	 *
+	 * Hooks into WordPress to change the admin login logo.
 	 */
 	public function __construct() {
 		\add_action( 'login_enqueue_scripts', [ $this, 'add_login_logo' ] );
 	}
 
 	/**
-	 * add_login_logo
+	 * Adds custom CSS to the WordPress login page to replace the default WordPress logo.
 	 *
-	 * Replace the WordPress Logo with the Customizer Logo on the wp-admin login screen
-	 *
-	 * @return void
+	 * Only adds the logo if a custom logo has been set in the theme customizer.
 	 */
 	public function add_login_logo(): void {
-		if ( $logo = \esc_url( \get_theme_mod( 'logo' ) ) ) : ?>
-			<style>
-				.login h1 a {
-					background-image: url(<?php echo $logo; ?>) !important;
-					width: 100% !important;
-					max-width: 300px !important;
-					-webkit-background-size: contain !important;
-					background-size: contain !important;
-				}
-			</style>
-		<?php endif;
+		$logo = \get_theme_mod( 'logo' );
+
+		if ( $logo ) {
+			echo $this->generate_login_logo_css( \esc_url( $logo ) );
+		}
+	}
+
+	/**
+	 * Generates CSS for customizing the login logo.
+	 *
+	 * @param string $logo_url URL of the custom logo.
+	 *
+	 * @return string CSS code for the custom login logo.
+	 */
+	protected function generate_login_logo_css( string $logo_url ): string {
+		return "
+            <style>
+                .login h1 a {
+                    background-image: url({$logo_url}) !important;
+                    width: 100% !important;
+                    max-width: 300px !important;
+                    -webkit-background-size: contain !important;
+                    background-size: contain !important;
+                }
+            </style>
+        ";
 	}
 }
 
