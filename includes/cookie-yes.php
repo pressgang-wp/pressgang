d9c586865b4c0255a61820c0928f9cb74f15b60e
Benedict <ben@benedict-wallis.com>
2024-01-12 15:36:36 +0000

    Update cookie-yes.php

--- includes/cookie-yes.php
+++ includes/cookie-yes.php
@@ -1,19 +1,11 @@
 <?php
-
-namespace Library;
-
-use function PressGang\__;
-use function PressGang\add_action;
-use function PressGang\get_theme_mod;
-use function PressGang\wp_enqueue_script;
-
 class CookieYes {
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		add_action( 'customize_register', array( $this, 'customizer' ) );
+		add_action( 'customize_register', array( $this, 'add_to_customizer' ) );
 		add_action( 'wp_enqueue_scripts', [ $this, 'cookieyes_header_script' ] );
 	}
 
@@ -22,11 +14,11 @@ class CookieYes {
 	 *
 	 * @param $wp_customize
 	 */
-	public function customizer( $wp_customize ) {
+	public function add_to_customizer( $wp_customize ): void {
 
-		if ( ! isset( $wp_customize->sections['cookieyes'] ) ) {
+		if ( ! $wp_customize->get_section( 'cookieyes' ) ) {
 			$wp_customize->add_section( 'cookieyes', array(
-				'title' => __( "CookieYes", THEMENAME ),
+				'title' => \_x( "CookieYes", 'Customizer', THEMENAME ),
 			) );
 		}
 
@@ -39,8 +31,8 @@ class CookieYes {
 		);
 
 		$wp_customize->add_control( new \WP_Customize_Control( $wp_customize, 'cookieyes-id', array(
-			'label'       => __( "CookieYes ID", THEMENAME ),
-			'description' => sprintf( __( "See %s" ), 'https://www.cookieyes.com/' ),
+			'label'       => \_x( "CookieYes ID", 'Customizer', THEMENAME ),
+			'description' => sprintf( \_x( "See %s",  'Customizer', THEMENAME),'https://www.cookieyes.com/' ),
 			'section'     => 'cookieyes',
 		) ) );
 	}
@@ -53,7 +45,7 @@ class CookieYes {
 	public function cookieyes_header_script() {
 
 		if ( $cookieyes_id = get_theme_mod( 'cookieyes-id' ) ) {
-			wp_enqueue_script( 'cookie-yes', "https://cdn-cookieyes.com/client_data/{$cookieyes_id}/script.js", [] );
+			\wp_enqueue_script( 'cookie-yes', "https://cdn-cookieyes.com/client_data/{$cookieyes_id}/script.js", [] );
 		}
 	}
 
