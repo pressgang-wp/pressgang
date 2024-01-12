f5d66c2d2ec5b55767efb4e1e187152ad08aeb57
Benedict <ben@benedict-wallis.com>
2024-01-12 12:11:22 +0000

    Improve Open Graph include

--- includes/opengraph.php
+++ includes/opengraph.php
@@ -3,62 +3,98 @@
 /**
  * Class OpenGraph
  *
+ * Manages the Open Graph meta tags for social media sharing.
+ *
+ * @see https://developers.facebook.com/docs/sharing/webmasters/#markup
  * @package PressGang
  */
 class OpenGraph {
 
 	/**
-	 * init
+	 * Constructor.
 	 *
+	 * Adds the Open Graph meta tags to the wp_head action.
 	 */
 	public function __construct() {
-		\add_action( 'wp_head', [ $this, 'fb_opengraph' ], 5 );
+		\add_action( 'wp_head', [ $this, 'add_meta_tags' ], 5 );
 	}
 
 	/**
-	 * fb_opengraph
+	 * Outputs the Open Graph meta tags.
 	 *
+	 * Gathers all necessary data and uses Timber to render the Open Graph meta tags.
 	 */
-	public function fb_opengraph() {
+	public function add_meta_tags() {
+		Timber::render( 'partials/open-graph.twig', [
+			'site_name'   => \apply_filters( 'pressgang_og_site_name', \get_bloginfo() ),
+			'title'       => \apply_filters( 'pressgang_og_title', $this->get_title() ),
+			'description' => \apply_filters( 'pressgang_og_description', $this->get_description() ),
+			'type'        => \apply_filters( 'pressgang_og_type', $this->get_type() ),
+			'url'         => \apply_filters( 'pressgang_og_url', $this->get_url() ),
+			'image'       => \apply_filters( 'pressgang_og_image', $this->get_image_url() ),
+		] );
+	}
 
-		$post = \Timber::get_post();
+	/**
+	 * Retrieves the URL of the Open Graph image.
+	 *
+	 * @return string The image URL.
+	 */
+	protected function get_image_url(): string {
+		$post    = \Timber::get_post();
+		$img_url = $post && \has_post_thumbnail( $post->ID )
+			? \wp_get_attachment_image_src( \get_post_thumbnail_id( $post->ID ), 'large' )[0]
+			: ( \get_theme_mod( 'logo' ) );
 
-		$img = $post && has_post_thumbnail( $post->ID )
-			? wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' )[0]
-			: ( get_theme_mod( 'og_img' )
-				? get_theme_mod( 'og_img' )
-				: esc_url( get_theme_mod( 'logo' ) ) );
+		return \apply_filters( 'og_image', $img_url );
+	}
 
-		$type = is_author() ? 'profile' : ( is_single() ? 'article' : 'website' );
+	/**
+	 * Determines the Open Graph type (e.g., article, website).
+	 *
+	 * @return string The Open Graph type.
+	 */
+	protected function get_type(): string {
+		return \is_author() ? 'profile' : ( is_single() ? 'article' : 'website' );
+	}
 
-		$description = PressGang\SEO\MetaDescriptionService::getMetaDescription();
+	/**
+	 * Retrieves the description for the Open Graph meta tag.
+	 *
+	 * @return string The description.
+	 */
+	protected function get_description(): string {
+		return PressGang\SEO\MetaDescriptionService::get_meta_description();
+	}
 
-		if ( is_tax() ) {
-			$url   = \get_term_link( \get_query_var( 'term' ), \get_query_var( 'taxonomy' ) );
-			$title = \single_term_title( '', false );
-		} elseif ( is_post_type_archive() ) {
-			$url   = \get_post_type_archive_link( \get_query_var( 'post_type' ) );
-			$title = \get_the_archive_title();
-		} else {
-			$url   = \get_permalink();
-			$title = \get_the_title();
+	/**
+	 * Retrieves the title for the Open Graph meta tag.
+	 *
+	 * @return string The title.
+	 */
+	protected function get_title(): string {
+		if ( \is_tax() ) {
+			return \single_term_title( '', false );
+		} elseif ( \is_post_type_archive() ) {
+			return \get_the_archive_title();
 		}
 
-		$url = rtrim( \esc_url( \apply_filters( 'og_url', $url ) ) );
-		if ( ! str_ends_with( $url, '/' ) ) {
-			$url .= '/'; // slash fixes Facebook Debugger "Circular Redirect Path"
+		return \get_the_title();
+	}
+
+	/**
+	 * Retrieves the URL for the Open Graph meta tag.
+	 *
+	 * @return string The URL.
+	 */
+	protected function get_url(): string {
+		if ( \is_tax() ) {
+			return \get_term_link( \get_query_var( 'term' ), \get_query_var( 'taxonomy' ) );
+		} elseif ( \is_post_type_archive() ) {
+			return \get_post_type_archive_link( \get_query_var( 'post_type' ) );
 		}
 
-		$open_graph = [
-			'site_name'   => \apply_filters( 'og_site_name', \get_bloginfo() ),
-			'title'       => \apply_filters( 'og_title', $title ),
-			'description' => \wp_strip_all_tags( \apply_filters( 'og_description', $description ) ),
-			'type'        => \esc_attr( apply_filters( 'og_type', $type ) ),
-			'url'         => \esc_url( 'og_url', $url ),
-			'image'       => \esc_url( \apply_filters( 'og_image', $img ) ),
-		];
-
-		\Timber::render( 'partials/open-graph.twig', [ 'open_graph' => $open_graph ] );
+		return \get_permalink();
 	}
 }
 
