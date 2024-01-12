32c2740a481367e76f1cbc44bf30ef3232173c36
Benedict <ben@benedict-wallis.com>
2024-01-12 15:37:18 +0000

    Improve comments and function naming convention

--- includes/logo-svg.php
+++ includes/logo-svg.php
@@ -3,39 +3,44 @@
 /**
  * Class LogoSvg
  *
- * TODO assume `composer require darylldoyle/safe-svg`
+ * Handles the addition of a logo SVG setting to the WordPress Customizer.
+ *
+ * Assumes that SVG is enabled in WordPress via an SVG sanitization plugin.
+ * Ideally via `composer require darylldoyle/safe-svg`
  *
  * @package PressGang
  */
 class LogoSvg {
 
+	/**
+	 * Constructor.
+	 *
+	 * Adds the add_to_customizer method to the 'customize_register' action hook in WordPress.
+	 */
 	public function __construct() {
-		\add_action( 'customize_register', [ $this, 'logo_svg' ] );
+		\add_action( 'customize_register', [ $this, 'add_to_customizer' ] );
 	}
 
 	/**
-	 * logo_svg
+	 * Add Logo SVG to Customizer.
 	 *
-	 * @param $wp_customize
+	 * Adds a new control for an SVG logo in the WordPress Customizer under the 'logo' section.
+	 *
+	 * @param \WP_Customize_Manager $wp_customize The WP_Customize_Manager instance.
 	 */
-	public static function logo_svg( $wp_customize ) {
+	public static function add_to_customizer( $wp_customize ): void {
 
 		if ( ! $wp_customize->get_section( 'logo' ) ) {
 			$wp_customize->add_section( 'logo', [
-				'title'    => __( "Logo", THEMENAME ),
+				'title'    => \_x( "Logo", 'Customizer', THEMENAME ),
 				'priority' => 30,
 			] );
 		}
 
-		$wp_customize->add_setting(
-			'logo_svg',
-			[
-				'default' => '',
-			]
-		);
+		$wp_customize->add_setting( 'logo_svg', [ 'default' => '' ] );
 
 		$wp_customize->add_control( new \WP_Customize_Image_Control( $wp_customize, 'logo_svg', [
-			'label'      => __( "Logo SVG", THEMENAME ),
+			'label'      => _x( "Logo SVG", 'Customizer', THEMENAME ),
 			'section'    => 'logo',
 			'extensions' => [ 'svg' ],
 		] ) );
