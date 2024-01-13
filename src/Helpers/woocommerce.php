f4e8f420145db8b34dd4131c2983d71252970fbc
Benedict <ben@benedict-wallis.com>
2024-01-13 09:06:29 +0000

    Update doc blocks and PHP 8 function calls

--- src/Helpers/woocommerce.php
+++ src/Helpers/woocommerce.php
@@ -6,10 +6,10 @@
  *
  * @return void
  */
-function timber_set_product( $post ) {
+function timber_set_product( $post ): void {
 	global $product;
 
-	if ( is_woocommerce() ) {
-		$product = wc_get_product( $post->ID );
+	if ( \is_woocommerce() ) {
+		$product = \wc_get_product( $post->ID );
 	}
 }
