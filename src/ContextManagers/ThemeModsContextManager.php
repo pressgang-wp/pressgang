e62f926867d2ef7cbfd057f0894133565566ef25
Benedict <ben@benedict-wallis.com>
2024-01-13 11:22:58 +0000

    Theme is object not array

--- src/ContextManagers/ThemeModsContextManager.php
+++ src/ContextManagers/ThemeModsContextManager.php
@@ -19,7 +19,7 @@ class ThemeModsContextManager implements ContextManagerInterface {
 	 * Adds WordPress theme modification settings (theme mods) to the Timber context.
 	 *
 	 * Retrieves all theme mods using the get_theme_mods function and adds each mod
-	 * to the Timber context in the 'theme' array. This allows theme mods to be easily accessed and used within
+	 * to the Timber context in the 'theme' object. This allows theme mods to be easily accessed and used within
 	 * the theme's Twig templates. The value of each mod can be filtered using the
 	 * 'pressgang_theme_mod_[key]' filter, allowing further customization.
 	 *
@@ -29,15 +29,11 @@ class ThemeModsContextManager implements ContextManagerInterface {
 	 */
 	public function add_to_context( array $context ): array {
 
-		$theme = $context['theme'];
-
 		if ( $theme_mods = \get_theme_mods() ) {
 			foreach ( $theme_mods as $key => $value ) {
-				$theme[ $key ] = \apply_filters( "pressgang_theme_mod_$key", $value );
+				$context['theme']->$key = \apply_filters( "pressgang_theme_mod_$key", $value );
 			}
 		}
-
-		$context['theme'] = $theme;
 
 		return $context;
 	}
