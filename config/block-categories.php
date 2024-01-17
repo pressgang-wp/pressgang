e2a9233de6c99353113321acf24493c39ed5bc73
Benedict <ben@benedict-wallis.com>
2024-01-17 10:07:13 +0000

    Refine block category registration and doc block

--- config/block-categories.php
+++ config/block-categories.php
@@ -5,17 +5,19 @@
  *
  * This configuration file defines an array of custom categories available for each block.
  *
- * The 'category' array allows categorization of the block in the Gutenberg editor and can be used
- * to group related blocks together.
+ * The 'category' array allows categorization of the block in the block editor and can be used
+ * to group related blocks together. The array keys provide the category slug used when registering the block,
+ * and the values provide the category title.
  *
- *     [
- *         'custom-category' => 'Custom Category',
- *     ],
- *     ... additional blocks ...
+ * Example:
+ * [
+ *     'slug' => 'Title',
+ *     // ...
+ * ]
  *
+ * @see https://developer.wordpress.org/reference/hooks/block_categories_all/
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  *
- * @var array
+ * @var array<string, string> An associative array of category slugs and their corresponding titles.
  */
-
 return [];
