dc54cb4d7c03c2881928bb07beed644173033f79
Benedict <ben@benedict-wallis.com>
2024-01-18 18:41:52 +0000

    Update woocommerce.php

--- woocommerce.php
+++ woocommerce.php
@@ -6,22 +6,21 @@ use PressGang\Controllers\WooCommerce\{
 	ProductController,
 	ProductsController
 };
-use PressGang\PressGang;
 
-$is_singular_product = \is_singular('product');
+$is_singular_product = \is_singular( 'product' );
 $is_product_category = \is_product_category();
-$shop_display = \get_option('woocommerce_shop_page_display');
+$shop_display        = \get_option( 'woocommerce_shop_page_display' );
 
 // Determine the controller based on the current page and shop display settings
-if ($is_singular_product) {
+if ( $is_singular_product ) {
 	$controller = ProductController::class;
-} elseif ($is_product_category) {
+} elseif ( $is_product_category ) {
 	$controller = ProductCategoryController::class;
-} elseif ($shop_display === 'subcategories') {
+} elseif ( $shop_display === 'subcategories' ) {
 	$controller = ProductCategoriesController::class;
 } else {
 	// Default to ProductsController or handle 'both' case here
 	$controller = ProductsController::class;
 }
 
-PressGang::render(controller: $controller);
+PressGang\PressGang::render( controller: $controller );
