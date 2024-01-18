f24d815ef0b133b57d18d7d5bcbc00e7f8078e50
Benedict <ben@benedict-wallis.com>
2024-01-18 16:48:36 +0000

    Update Controller doc blocks and function defs

--- src/Controllers/WooCommerce/ProductCategoryController.php
+++ src/Controllers/WooCommerce/ProductCategoryController.php
@@ -2,37 +2,42 @@
 
 namespace PressGang\Controllers\WooCommerce;
 
-use PressGang\TaxonomyController;
+use PressGang\Controllers\TaxonomyController;
 
 /**
- * Class ProductsController
+ * Class ProductCategoryController
+ *
+ * Controller for WooCommerce product categories, extending the base TaxonomyController.
+ * This class provides WooCommerce-specific functionality for category archives.
  *
  * @package PressGang
  */
 class ProductCategoryController extends TaxonomyController {
 
-	protected $category = null;
-
 	/**
-	 * __construct
+	 * ProductCategoryController constructor.
 	 *
-	 * WCProductController constructor
+	 * Initializes the controller for WooCommerce product categories with a specified template.
 	 *
-	 * @param string $template
+	 * @param string $template The template file to use for rendering the product category archive. Defaults to 'woocommerce/archive.twig'.
 	 */
-	public function __construct( $template = 'woocommerce/archive.twig' ) {
+	public function __construct( $template = 'woocommerce/product-categories.twig' ) {
 		parent::__construct( $template );
 	}
 
 	/**
-	 * get_context
+	 * Retrieves and prepares the context data for rendering the product category archive.
 	 *
+	 * This method extends the base get_context method, adding specific data like the current category
+	 * and the category title to the context for rendering in the Twig template.
+	 *
+	 * @return array The context array with additional data for the product category archive.
 	 */
-	public function get_context() {
+	protected function get_context(): array {
 		parent::get_context();
 
-		$this->context['category'] = $this->get_taxonomy();
-		$this->context['title']    = single_term_title( '', false );
+		$this->context['category'] = $this->get_term();
+		$this->context['title']    = \single_term_title( '', false );
 
 		return $this->context;
 	}
