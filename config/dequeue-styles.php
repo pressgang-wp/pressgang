2ee7dacb586b5e4e761dd724ae07ed6a8977ab99
Benedict <ben@benedict-wallis.com>
2024-03-07 16:06:21 +0000

    Create dequeue-styles.php

---
+++ config/dequeue-styles.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Dequeue Styles Configuration
+ *
+ * This configuration file defines the Stylesheets to be dequeued.
+ *
+ * Example configuration array:
+ *      return [
+ *          'style-tag',
+ *      ];
+ *
+ * @var array
+ */
+return [];
