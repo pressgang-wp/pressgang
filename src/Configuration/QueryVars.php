c2955b37176064188991ea2fe4b5f3df318cb7e7
Benedict <ben@benedict-wallis.com>
2024-05-14 11:54:45 +0100

    Fix config in QueryVars

--- src/Configuration/QueryVars.php
+++ src/Configuration/QueryVars.php
@@ -20,19 +20,25 @@ class QueryVars extends ConfigurationSingleton {
 	protected array $config;
 
 	/**
+	 *
 	 * This method registers the custom query variables defined in the configuration array
 	 * to the WordPress query system using the 'query_vars' filter.
 	 *
 	 * @param array $config An array of query variables to be added.
 	 *                      Each element in the array should be a string representing a query variable name.
+	 *
 	 * @return void
 	 */
 	public function initialize( array $config ): void {
-		\add_filter('query_vars', function($vars) {
-			foreach ($this->config as $var) {
+
+		$this->config = $config;
+
+		\add_filter( 'query_vars', function ( $vars ) {
+			foreach ( $this->config as $var ) {
 				$vars[] = $var;
 			}
+
 			return $vars;
-		});
+		} );
 	}
 }
