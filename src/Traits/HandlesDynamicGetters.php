3c20226aaf7bb316b2a0cb45f6be08d8aa82ca07
Benedict <ben@benedict-wallis.com>
2026-02-07 20:16:45 +0000

    Add HandlesDynamicGetters trait
    Introduce PressGang\Traits\HandlesDynamicGetters to allow model classes to expose get_{property}() methods as normal properties. The trait intercepts __get and meta calls, detects get_{field} methods, and invokes them using ReflectionMethod to resolve and pass any default parameter values. This enables Twig (and PHP property access) to call custom getters on Timber 2 model classes without changing existing call sites.

---
+++ src/Traits/HandlesDynamicGetters.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace PressGang\Traits;
+
+use ReflectionMethod;
+
+/**
+ * Trait HandlesDynamicGetters
+ *
+ * Bridges PHP getter methods to Twig property access for Timber 2 model classes.
+ * Models using this trait can define `get_{property}()` methods that are automatically
+ * called when accessing `$model->property` in PHP or `{{ model.property }}` in Twig.
+ *
+ * @package PressGang\Traits
+ */
+trait HandlesDynamicGetters {
+
+	/**
+	 * Intercepts property access to check for custom getter methods.
+	 *
+	 * @param string $field
+	 *
+	 * @return mixed
+	 */
+	public function __get( $field ) {
+		if ( $this->has_custom_getter( $field ) ) {
+			return $this->call_custom_getter( $field );
+		}
+
+		return parent::__get( $field );
+	}
+
+	/**
+	 * Intercepts meta calls to check for custom getter methods.
+	 *
+	 * @param string $field_name
+	 * @param array  $args
+	 *
+	 * @return mixed
+	 */
+	public function meta( $field_name = '', $args = [] ) {
+		if ( $field_name && $this->has_custom_getter( $field_name ) ) {
+			return $this->call_custom_getter( $field_name );
+		}
+
+		return parent::meta( $field_name, $args );
+	}
+
+	/**
+	 * Checks whether a custom getter method exists for the given field.
+	 *
+	 * @param string $field
+	 *
+	 * @return bool
+	 */
+	protected function has_custom_getter( string $field ): bool {
+		return method_exists( $this, "get_{$field}" );
+	}
+
+	/**
+	 * Calls the custom getter method for the given field.
+	 *
+	 * @param string $field
+	 *
+	 * @return mixed
+	 */
+	protected function call_custom_getter( string $field ): mixed {
+		$method = "get_{$field}";
+
+		$reflection = new ReflectionMethod( $this, $method );
+		$parameters = $reflection->getParameters();
+		$resolved   = $this->default_parameter_resolution( $parameters );
+
+		return $this->$method( ...$resolved );
+	}
+
+	/**
+	 * Resolves default values for method parameters.
+	 *
+	 * @param \ReflectionParameter[] $parameters
+	 *
+	 * @return array
+	 */
+	protected function default_parameter_resolution( array $parameters ): array {
+		$resolved = array();
+
+		foreach ( $parameters as $param ) {
+			if ( $param->isDefaultValueAvailable() ) {
+				$resolved[] = $param->getDefaultValue();
+			}
+		}
+
+		return $resolved;
+	}
+}
